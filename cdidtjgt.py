60901cc70f04a0b0f27a22251fbce07dd37ec10d
Kevin林凯子 <70473845+Kevinistic@users.noreply.github.com>
2024-09-25 17:27:13 +0700

    -

--- cdidtjgt.py
+++ cdidtjgt.py
@@ -3,6 +3,7 @@
 
 # alamak
 import tkinter as tk # gui
+from tkinter import ttk
 import time # importing time from a land faraway
 import math
 from tkinter import filedialog
@@ -24,30 +25,10 @@ d = { # important variables in a dictionary named "d" (stfu ik it's bad naming b
     "time_pause_difference": 0,
     "time_pause_total": 0,
     "runtime_seconds": 0,
+    "entry2_input": "",
     "flag_pause": False,
+    "flag_time_pause": False,
     "flag_time": True,
-    "lang": True,
-}
-
-language_dict = {
-    "EN": {
-        "label2_11": "Enter your current money:",
-        "label2_21": "Money before start: ",
-        "label2_31": "Money earned so far: ",
-        "label2_41": "Money per hour: ",
-        "label2_51": "Time taken since last update: ",
-        "label2_61": "Program runtime: ",
-        "button2_4": "ID",
-    },
-    "ID": {
-        "label2_11": "Masukkan uang anda saat ini:",
-        "label2_21": "Uang di awal: ",
-        "label2_31": "Uang yang didapatkan: ",
-        "label2_41": "Uang per jam: ",
-        "label2_51": "Waktu sejak terakhir kali update: ",
-        "label2_61": "Waktu program berjalan: ",
-        "button2_4": "EN",
-    }
 }
 
 def ms(n): # float to minutes and seconds
@@ -60,20 +41,44 @@ def hms(n): # float to hours, minutes and seconds
     m = int(rs // 60)
     s = round(rs % 60)
     return f"{h}h {m}m {s}s"
-def start_pause():
+def pause():
     global d
-    d["time_pause_start"] = time.time()
-    d["flag_pause"] = True
-    show_frame(frame3)
-def stop_pause():
+    if not d["flag_pause"]:
+        d["time_pause_start"] = time.time()
+        d["flag_time_pause"] = True
+        d["flag_pause"] = True
+        entry2.delete(0, tk.END)  # clear entry
+        entry2.insert(0, "Program paused!")  # insert
+        entry2.config(state='readonly')  # temp lock
+        button2_1.config(state="disabled")
+        button2_2.config(state="disabled")
+        button2_3.config(text="Resume")
+    else:
+        d["time_pause_stop"] = time.time()
+        d["time_pause_difference"] = d["time_pause_stop"] - d["time_pause_start"]
+        d["time_pause_total"] += d["time_pause_difference"]
+        d["flag_pause"] = False
+        entry2.config(state='normal')  # unlock
+        entry2.delete(0, tk.END)  # clear entry
+        entry2.insert(0, d["entry2_input"])
+        button2_1.config(state="normal")
+        button2_2.config(state="normal")
+        button2_3.config(text="Pause")
+def invalid_entry2():
     global d
-    d["time_pause_stop"] = time.time()
-    d["time_pause_difference"] = d["time_pause_stop"] - d["time_pause_start"]
-    d["time_pause_total"] += d["time_pause_difference"]
-    show_frame(frame2)
-def invalidentry(n):
-    n.config(bg="red")
-    root.after(100, lambda: n.config(bg="white"))
+    d["entry2_input"] = entry2.get()
+    entry2.delete(0, tk.END)  # clear entry
+    entry2.insert(0, "Invalid input!")  # insert
+    entry2.config(state='readonly')  # temp lock
+    button2_2.config(state="disabled")
+    button2_3.config(state="disabled")
+    root.after(500, restore_entry)
+def restore_entry():
+    entry2.config(state='normal')  # unlock
+    entry2.delete(0, tk.END)  # clear entry
+    entry2.insert(0, d["entry2_input"])
+    button2_2.config(state="normal")
+    button2_3.config(state="normal")
 def show_frame(frame): # switcharoo!
     frame.tkraise()
 def process_input(): # PLEASE DO NOT FUCK AROUND WITH THIS FUNCTION UNLESS YOU KNWO WHAT YOURE DOING!!!!!!!!!!
@@ -98,9 +103,9 @@ def process_input(): # PLEASE DO NOT FUCK AROUND WITH THIS FUNCTION UNLESS YOU K
                 d["time_recent"] = time_input
             else:
                 time_input = time.time()
-                if d["flag_pause"]:
+                if d["flag_time_pause"]:
                     d["time_difference"] = time_input - d["time_recent"] - d["time_pause_difference"]
-                    d["flag_pause"] = False
+                    d["flag_time_pause"] = False
                 else:
                     d["time_difference"] = time_input - d["time_recent"]
                 d["time_recent"] = time_input
@@ -117,12 +122,12 @@ def process_input(): # PLEASE DO NOT FUCK AROUND WITH THIS FUNCTION UNLESS YOU K
             d["money_recent"] = money_input
         else:
             label2_32.config(text=f"{d['result_memory']}")
-            invalidentry(entry2)
+            invalid_entry2()
             d["result_placeholder"] = d["result_memory"]
         label2_22.config(text=f"{d['money_memory']:,}")
     except ValueError:
         label2_32.config(text=f"{d['result_memory']}")
-        invalidentry(entry2)
+        invalid_entry2()
 def is_close(a, b, tol=1e-9):
     return abs(a - b) < tol
 def runtimef():
@@ -136,19 +141,14 @@ def go():
 def reset():
     global d
     for key in d.keys():
-        if key in ["flag_time", "runtime_seconds", "lang", "flag_pause", "time_recent"]:
+        if key in ["flag_time", "runtime_seconds", "flag_time_pause", "time_recent", "entry2_input"]:
             continue
         d[key] = 0
+    entry2.delete(0, tk.END)
     label2_22.config(text=f"{d['money_memory']}")
     label2_32.config(text=f"{d['result_memory']}")
     label2_42.config(text=f"{round(d['rate_hour'], 2)}")
     label2_52.config(text=f"{ms(d['time_difference'])}")
-def language():
-    global d
-    lang_code = "ID" if d["lang"] else "EN"
-    for key, sentence in language_dict[lang_code].items():
-        globals()[key].config(text=sentence)
-    d["lang"] = not d["lang"]
 def on_focus_in(n): # opacity change when not in focus
     root.wm_attributes("-alpha", 1)
 def on_focus_out(n):
@@ -167,74 +167,57 @@ root.bind("<FocusIn>", on_focus_in)
 root.bind("<FocusOut>", on_focus_out)
 frame1 = tk.Frame(root) # frames for gui
 frame2 = tk.Frame(root)
-frame3 = tk.Frame(root)
 frame1.grid(row=0, column=0, sticky='nsew') # grids for frames
 frame2.grid(row=0, column=0, sticky='nsew')
-frame3.grid(row=0, column=0, sticky='nsew')
 
 # frame 1 content
-label1 = tk.Label(frame1, text="""Welcome! This program is created to assist your CDID Truck Job grinding. For any inquiries, contact @cn3z on Discord. Click the button below to start.""",
+label1 = ttk.Label(frame1, text="""Welcome! This program is created to assist your CDID Truck Job grinding. For any inquiries, contact @cn3z on Discord. Click the button below to start.""",
                   wraplength=320,
                   justify=tk.LEFT
                   )
 label1.pack()
 
-button1 = tk.Button(frame1, text="Here!", command=lambda: go())
+button1 = ttk.Button(frame1, text="Here!", command=lambda: go(), width=5)
 button1.pack()
 
 # frame 2 content
-labelentry = tk.Frame(frame2)
-labelentry.grid(row=0, column=0, sticky='w')
-label2_11 = tk.Label(labelentry, text="Enter your current money:", justify=tk.LEFT)
+label_frame = tk.Frame(frame2)
+label_frame.grid(row=0, column=0, sticky='w')
+label2_11 = ttk.Label(label_frame, text="Enter your current money:", justify=tk.LEFT)
+label2_21 = ttk.Label(label_frame, text="Money before start: ", justify=tk.LEFT)
+label2_31 = ttk.Label(label_frame, text="Money earned so far: ", justify=tk.LEFT)
+label2_41 = ttk.Label(label_frame, text="Money per hour: ", justify=tk.LEFT)
+label2_51 = ttk.Label(label_frame, text="Time taken since last update: ", justify=tk.LEFT)
+label2_61 = ttk.Label(label_frame, text="Program runtime: ", justify=tk.LEFT)
 label2_11.grid(row=0, column=0, sticky='w')
+label2_21.grid(row=1, column=0, sticky='w')
+label2_31.grid(row=2, column=0, sticky='w')
+label2_41.grid(row=3, column=0, sticky='w')
+label2_51.grid(row=4, column=0, sticky='w')
+label2_61.grid(row=5, column=0, sticky='w')
 
-entry2 = tk.Entry(labelentry, justify=tk.LEFT)
+entry2 = ttk.Entry(label_frame, justify=tk.LEFT)
 entry2.grid(row=0, column=1, sticky='w')
 
-label_frame = tk.Frame(frame2)
-label_frame.grid(row=1, column=0, sticky='w')
-label2_21 = tk.Label(label_frame, text="Money before start: ", justify=tk.LEFT)
-label2_31 = tk.Label(label_frame, text="Money earned so far: ", justify=tk.LEFT)
-label2_41 = tk.Label(label_frame, text="Money per hour: ", justify=tk.LEFT)
-label2_51 = tk.Label(label_frame, text="Time taken since last update: ", justify=tk.LEFT)
-label2_61 = tk.Label(label_frame, text="Program runtime: ", justify=tk.LEFT)
-label2_21.grid(row=0, column=0, sticky='w')
-label2_31.grid(row=1, column=0, sticky='w')
-label2_41.grid(row=2, column=0, sticky='w')
-label2_51.grid(row=3, column=0, sticky='w')
-label2_61.grid(row=4, column=0, sticky='w')
+label2_22 = ttk.Label(label_frame, text=f"{d['money_memory']}", justify=tk.LEFT)
+label2_32 = ttk.Label(label_frame, text=f"{d['result_memory']}", justify=tk.LEFT)
+label2_42 = ttk.Label(label_frame, text=f"{d['rate_hour']}", justify=tk.LEFT)
+label2_52 = ttk.Label(label_frame, text=f"{ms(d['time_difference'])}", justify=tk.LEFT)
+label2_62 = ttk.Label(label_frame, text="", justify=tk.LEFT)
+label2_22.grid(row=1, column=1, sticky='w')
+label2_32.grid(row=2, column=1, sticky='w')
+label2_42.grid(row=3, column=1, sticky='w')
+label2_52.grid(row=4, column=1, sticky='w')
+label2_62.grid(row=5, column=1, sticky='w')
 
-label2_22 = tk.Label(label_frame, text=f"{d['money_memory']}", justify=tk.LEFT)
-label2_32 = tk.Label(label_frame, text=f"{d['result_memory']}", justify=tk.LEFT)
-label2_42 = tk.Label(label_frame, text=f"{d['rate_hour']}", justify=tk.LEFT)
-label2_52 = tk.Label(label_frame, text=f"{ms(d['time_difference'])}", justify=tk.LEFT)
-label2_62 = tk.Label(label_frame, text="", justify=tk.LEFT)
-label2_22.grid(row=0, column=1, sticky='w')
-label2_32.grid(row=1, column=1, sticky='w')
-label2_42.grid(row=2, column=1, sticky='w')
-label2_52.grid(row=3, column=1, sticky='w')
-label2_62.grid(row=4, column=1, sticky='w')
-
-button_frame = tk.Frame(label_frame)
-button_frame.grid(row=5, column=0, sticky='w')
-button2_1 = tk.Button(button_frame, text="Update", command=process_input)
-button2_2 = tk.Button(button_frame, text="Restart", command=reset)
-button2_3 = tk.Button(button_frame, text="Pause", command=start_pause)
-button2_4 = tk.Button(button_frame, text="ID", command=language)
+button_frame = tk.Frame(frame2)
+button_frame.grid(row=1, column=0, sticky='w')
+button2_1 = ttk.Button(button_frame, text="Update", command=process_input, width=7)
+button2_2 = ttk.Button(button_frame, text="Restart", command=reset, width=7)
+button2_3 = ttk.Button(button_frame, text="Pause", command=pause, width=7)
 button2_1.grid(row=0, column=0, sticky='w')
 button2_2.grid(row=0, column=1, sticky='w')
 button2_3.grid(row=0, column=2, sticky='w')
-button2_4.grid(row=0, column=3, sticky='w')
-
-# frame 3 content
-label3 = tk.Label(frame3, text="The program is currently paused. Click the button below to continue.",
-                  wraplength=320,
-                  justify=tk.LEFT
-                  )
-label3.pack()
-
-button3 = tk.Button(frame3, text="Here!", command=stop_pause)
-button3.pack()
 
 # do NOT delete
 show_frame(frame1)
